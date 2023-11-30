84768d6e4f13e822790acc27807ecb6d0936370c
Jonas Kubesch <jonas.kubesch@gmail.com>
2023-11-30 20:22:24 +0100

    Init Day 1

---
+++ Day_1/Day1_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class Day1_1
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = new StreamReader("input_1.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+}
