b9da2cfcd6ad52a5ca76c40e8a7ea23d4ec5e7e6
Jonas Kubesch <fhs45890@fh-salzburg.ac.at>
2023-12-02 13:53:55 +0100

    Day2_1

---
+++ Day_2/Program.cs
@@ -0,0 +1,78 @@
+﻿
+public class Day2
+{
+    static void Main()
+    {
+        Part1();
+    }
+
+    static void Part1()
+    {
+        using(StreamReader reader = new StreamReader("Input_1.txt"))
+        {
+            List<string> lines = new List<string>();
+            string line;
+
+            int solution1 = 0;
+            int lineCount = 1;
+
+            while(!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                var firstColonIndex = line.IndexOf(':');
+                line = line.Remove(0,firstColonIndex + 1);
+
+                bool isValid = true;
+
+                //line now contains only sets
+
+                var gamesInLine = line.Split(';');
+
+                foreach(var game in gamesInLine)
+                {
+                   var blueIndex = game.IndexOf("blue");
+                   var redIndex = game.IndexOf("red");
+                   var greenIndex = game.IndexOf("green");
+
+                    int blueCount = 0;
+                    int redCount = 0;
+                    int greenCount = 0;
+
+                   if(blueIndex != -1)
+                   {
+                    var number = game.Substring(blueIndex - 3, 3);
+                    blueCount = int.Parse(number);
+                   }
+
+                   
+                   if(redIndex != -1)
+                   {
+                    var number = game.Substring(redIndex - 3, 3);
+                    redCount = int.Parse(number);
+                   }
+                   
+                   if(greenIndex != -1)
+                   {
+                    var number = game.Substring(greenIndex - 3, 3);
+                    greenCount = int.Parse(number);
+                   }
+
+                   if(blueCount > 14 || greenCount > 13 || redCount > 12)
+                   {
+                    isValid = false;
+                    break;
+                   }
+                }
+
+                if(isValid)
+                {
+                    solution1 += lineCount;
+                }
+
+                lineCount++;
+            }
+
+            Console.WriteLine($"Solution 1 = {solution1}");
+        }
+    }
+}
