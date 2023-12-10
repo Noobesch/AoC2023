7e6e9b5c68576f63afbb9e8ab5df310102613299
Jonas Kubesch <fhs45890@fh-salzburg.ac.at>
2023-12-10 14:36:12 +0100

    Day10_2 Graphical take

--- Day_10/Program.cs
+++ Day_10/Program.cs
@@ -5,7 +5,7 @@
         string path = "Input_1.txt";
         // path = "../../../Input_1.txt";
         Part1(path);
-        // Part2(path);
+        Part2(path);
     }
 
     static void Part1(string path)
@@ -130,7 +130,7 @@
                 inputList[lineIndex] = line;
             }
 
-            using (StreamWriter writer = new StreamWriter("Outputx.txt"))
+            using (StreamWriter writer = new StreamWriter("Output1.txt"))
             {
                 for (var lineIndex = 0; lineIndex < inputList.Count; lineIndex++)
                 {
@@ -144,6 +144,216 @@
             }
 
             Console.WriteLine($"Loop length is {(loopLength + 1) / 2}");
+        }
+    }
+    static void Part2(string path)
+    {
+        Dictionary<char, (int row, int column)[]> mappingDict = new Dictionary<char, (int row, int column)[]>();
+
+        mappingDict.Add('|', new (int row, int column)[] { (-1, 0), (1, 0) });
+        mappingDict.Add('-', new (int row, int column)[] { (0, 1), (0, -1) });
+        mappingDict.Add('L', new (int row, int column)[] { (-1, 0), (0, 1) });
+        mappingDict.Add('J', new (int row, int column)[] { (-1, 0), (0, -1) });
+        mappingDict.Add('7', new (int row, int column)[] { (1, 0), (0, -1) });
+        mappingDict.Add('F', new (int row, int column)[] { (1, 0), (0, 1) });
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            List<char[]> inputList = new List<char[]>();
+
+            (int row, int column) startPosition = (-1, -1);
+            int rowCounter = 0;
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine().Replace("\r\n", "");
+
+                if (line.Contains('S'))
+                {
+                    startPosition = (rowCounter, line.IndexOf('S'));
+                }
+                inputList.Add(line.ToCharArray());
+
+                rowCounter++;
+            }
+
+            int rowLength = inputList.Count;
+            int columnLength = inputList[0].Length;
+
+            for (var runThroughs = 0; runThroughs < 10; runThroughs++)
+            {
+                for (var rowIndex = 0; rowIndex < inputList.Count; rowIndex++)
+                {
+                    for (var columnIndex = 0; columnIndex < inputList[0].Length; columnIndex++)
+                    {
+                        char symbol = inputList[rowIndex][columnIndex];
+
+                        if (!mappingDict.Keys.Contains(symbol))
+                        {
+                            continue;
+                        }
+
+                        foreach (var step in mappingDict[symbol])
+                        {
+                            (int row, int column) nextCoords = (rowIndex + step.row, columnIndex + step.column);
+
+                            if (nextCoords.row < 0 || nextCoords.row >= rowLength ||
+                            nextCoords.column < 0 || nextCoords.column >= columnLength)
+                            {
+                                inputList[rowIndex][columnIndex] = '.';
+                                continue;
+                            }
+
+
+                            char nextSymbol = inputList[rowIndex + step.row][columnIndex + step.column];
+
+                            if (nextSymbol == '.')
+                            {
+                                inputList[rowIndex][columnIndex] = '.';
+                                continue;
+                            }
+                            else if (nextSymbol == 'S')
+                            {
+                                continue;
+                            }
+
+                            (int row, int column)[] steps = mappingDict[nextSymbol];
+
+                            if (!steps.Contains((-step.row, -step.column)))
+                            {
+                                inputList[rowIndex][columnIndex] = '.';
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+            for (var lineIndex = 0; lineIndex < inputList.Count; lineIndex++)
+            {
+                var line = inputList[lineIndex];
+                bool firstCharFound = false;
+
+                for (var charIndex = 0; charIndex < line.Length; charIndex++)
+                {
+                    char character = line[charIndex];
+                    if (!firstCharFound && character == '.')
+                    {
+                        line[charIndex] = ' ';
+                        continue;
+                    }
+
+                    if (character != '.' && character != ' ')
+                    {
+                        firstCharFound = true;
+                    }
+
+                    switch (character)
+                    {
+                        case '|':
+                            line[charIndex] = '|';
+                            break;
+                        case '-':
+                            line[charIndex] = '─';
+                            break;
+                        case 'L':
+                            line[charIndex] = '└';
+                            break;
+                        case 'J':
+                            line[charIndex] = '┘';
+                            break;
+                        case '7':
+                            line[charIndex] = '┐';
+                            break;
+                        case 'F':
+                            line[charIndex] = '┌';
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                inputList[lineIndex] = line;
+            }
+
+            for (var lineIndex = 0; lineIndex < inputList.Count; lineIndex++)
+            {
+                var line = inputList[lineIndex];
+                bool firstCharFound = false;
+
+                for (var charIndex = 0; charIndex < line.Length; charIndex++)
+                {
+                    char character = line[charIndex];
+                    if (!firstCharFound && character == '.')
+                    {
+                        line[charIndex] = ' ';
+                        continue;
+                    }
+
+                    if (character != '.' && character != ' ')
+                    {
+                        firstCharFound = true;
+                    }
+                }
+                inputList[lineIndex] = line;
+            }
+
+            for (var lineIndex = 0; lineIndex < inputList.Count; lineIndex++)
+            {
+                var line = inputList[lineIndex];
+                bool firstCharFound = false;
+
+                for (var charIndex = line.Length - 1; charIndex >= 0; charIndex--)
+                {
+                    char character = line[charIndex];
+                    if (!firstCharFound && character == '.')
+                    {
+                        line[charIndex] = ' ';
+                        continue;
+                    }
+
+                    if (character != '.' && character != ' ')
+                    {
+                        firstCharFound = true;
+                    }
+                }
+                inputList[lineIndex] = line;
+            }
+
+
+            for (var charIndex = inputList[0].Length - 1; charIndex >= 0; charIndex--)
+            {
+                bool firstCharFound = false;
+
+                for (var lineIndex = 0; lineIndex < inputList.Count; lineIndex++)
+                {
+
+                    char character = inputList[lineIndex][charIndex];
+                    if (!firstCharFound && character == '.')
+                    {
+                        inputList[lineIndex][charIndex] = ' ';
+                        continue;
+                    }
+
+                    if (character != '.' && character != ' ')
+                    {
+                        firstCharFound = true;
+                    }
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("Output2.txt"))
+            {
+                for (var lineIndex = 0; lineIndex < inputList.Count; lineIndex++)
+                {
+                    for (var charIndex = 0; charIndex < inputList[0].Length; charIndex++)
+                    {
+                        writer.Write(inputList[lineIndex][charIndex]);
+                    }
+                    writer.WriteLine();
+                }
+
+            }
         }
     }
 }
