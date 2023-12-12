0a0932d9bf904fd4291369c2b9fd7fe35408213e
Jonas Kubesch <fhs45890@fh-salzburg.ac.at>
2023-12-12 10:18:03 +0100

    Day12_01 done

--- Day_12/Program.cs
+++ Day_12/Program.cs
@@ -5,8 +5,9 @@ public class Day12
     static void Main()
     {
         string path = "Input_1.txt";
-        path = "../../../Input_1.txt";
+        // path = "../../../Input_1.txt";
         Part1(path);
+        Part2(path);
     }
 
     static void Part1(string path)
@@ -34,21 +35,83 @@ public class Day12
                 instructionList.Add(lineList);
             }
 
+            int solution1 = 0;
+
             for (var lineIndex = 0; lineIndex < riddleList.Count; lineIndex++)
             {
                 string riddleInput = riddleList[lineIndex];
                 List<int> instructionInput = instructionList[lineIndex];
 
                 int lineSolution = CalculatePossibilities(riddleInput, "", instructionInput);
-                Console.WriteLine($"Line {lineIndex} is {lineSolution}");
+                solution1 += lineSolution;
             }
+
+            Console.WriteLine($"Solution 1 is {solution1}");
+
+        }
+    }
+
+    static void Part2(string path)
+    {
+        List<string> riddleList = new List<string>();
+        List<List<int>> instructionList = new List<List<int>>();
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            while (!reader.EndOfStream)
+            {
+                var splitLine = reader.ReadLine().Split(" ");
+
+                string riddle = "";
+                for(var i = 0; i < 5; i++)
+                {
+                    riddle += splitLine[0];
+                    riddle += "?";
+                }
+
+                riddle = riddle.Remove(riddle.Length -1, 1);
+                riddleList.Add(riddle);
+
+                var splitNumbers = splitLine[1].Split(',');
+
+                List<int> lineList = new List<int>();
+
+                foreach (var number in splitNumbers)
+                {
+                    lineList.Add(int.Parse(number));
+                }
+
+                List<int> instruction = new List<int>();
+                for(var i = 0; i < 5; i++)
+                {
+                    instruction.AddRange(lineList);
+                }
+                instructionList.Add(instruction);
+            }
+
+            int solution1 = 0;
+
+            for (var lineIndex = 0; lineIndex < riddleList.Count; lineIndex++)
+            {
+                string riddleInput = riddleList[lineIndex];
+                List<int> instructionInput = instructionList[lineIndex];
+
+                int lineSolution = CalculatePossibilities(riddleInput, "", instructionInput);
+                solution1 += lineSolution;
+
+                Console.WriteLine($"Solution for line {lineIndex + 1} is {lineSolution}");
+            }
+
+            Console.WriteLine($"Solution 1 is {solution1}");
+
         }
     }
 
     static bool IsValidCombination(string solution, List<int> instructions)
     {
- 
+
         List<int> possibleSolutions = new List<int>();
+        possibleSolutions.Add(0);
         int brokenCounter = 0;
 
         for (var i = 0; i < solution.Length; i++)
@@ -57,26 +120,27 @@ public class Day12
 
             if (character == '#')
             {
-                brokenCounter++;
+                possibleSolutions[possibleSolutions.Count - 1]++;
             }
-            if ((character == '.' && brokenCounter != 0) || i == solution.Length -1)
+            else
             {
-                possibleSolutions.Add(brokenCounter);
-                brokenCounter = 0;
+                possibleSolutions.Add(0);
             }
         }
-        
-        if(instructions.Count != possibleSolutions.Count)
+
+        possibleSolutions.RemoveAll(x => x == 0);
+
+        if (instructions.Count != possibleSolutions.Count)
         {
             return false;
         }
 
-        for(var i = 0; i < instructions.Count; i++)
+        for (var i = 0; i < instructions.Count; i++)
         {
             int instruction = instructions[i];
             int possibleSolution = possibleSolutions[i];
 
-            if(instruction != possibleSolution)
+            if (instruction != possibleSolution)
             {
                 return false;
             }
