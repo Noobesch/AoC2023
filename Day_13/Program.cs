eeaacf46dd5541df534bfa05bb9b79bb203ed7ea
Jonas Kubesch <jonas.kubesch@gmail.com>
2023-12-13 09:54:46 +0100

    Day_13 Task 2, Lesekompetenz nicht vorhanden

--- Day_13/Program.cs
+++ Day_13/Program.cs
@@ -7,7 +7,7 @@ public class Day13
         string path = "Input_1.txt";
         path = "../../../Input_1.txt";
         Part1(path);
-        // Part2(path);
+        Part2(path);
     }
 
     static void Part1(string path)
@@ -52,14 +52,67 @@ public class Day13
 
         long solution1 = 0;
 
-        solution1 += CalculateMirrors(rowListList, true);
-        solution1 += CalculateMirrors(columnListList, false);
+        solution1 += CalculateMirrors1(rowListList, true);
+        solution1 += CalculateMirrors1(columnListList, false);
 
 
         Console.WriteLine($"Solution 1 ist {solution1}");
     }
 
-    static long CalculateMirrors(List<List<string>> ListList, bool isRow)
+    static void Part2(string path)
+    {
+        List<List<string>> rowListList = new List<List<string>>();
+        List<List<string>> columnListList = new List<List<string>>();
+
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            List<string> rowList = new List<string>();
+            List<string> columnList = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine().Replace("\r\n", "");
+
+                if (line == "" || reader.EndOfStream)
+                {
+                    if (reader.EndOfStream)
+                    {
+                        rowList.Add(line);
+                    }
+                    rowListList.Add(rowList);
+
+                    for (var columnIndex = 0; columnIndex < rowList[0].Length; columnIndex++)
+                    {
+                        string column = "";
+                        for (var rowIndex = 0; rowIndex < rowList.Count; rowIndex++)
+                        {
+                            column += rowList[rowIndex][columnIndex];
+                        }
+                        columnList.Add(column);
+                    }
+
+                    columnListList.Add(columnList);
+
+                    rowList = new List<string>();
+                    columnList = new List<string>();
+                }
+                else
+                {
+                    rowList.Add(line);
+                }
+            }
+        }
+
+        long solution2 = 0;
+
+        solution2 += CalculateMirrors2(rowListList, true);
+        solution2 += CalculateMirrors2(columnListList, false);
+
+
+        Console.WriteLine($"Solution 2 ist {solution2}");
+    }
+
+    static long CalculateMirrors1(List<List<string>> ListList, bool isRow)
     {
         long partSolution = 0;
         int listCounter = 0;
@@ -102,6 +155,71 @@ public class Day13
                         break;
                     }
 
+                }
+            }
+            partSolution += tempSolution;
+        }
+
+        return partSolution;
+    }
+
+    static long CalculateMirrors2(List<List<string>> ListList, bool isRow)
+    {
+        long partSolution = 0;
+        int listCounter = 0;
+        foreach (var List in ListList)
+        {
+            listCounter++;
+            long tempSolution = 0;
+
+
+            for (var smudgeRowIndex = 0; smudgeRowIndex < List.Count; smudgeRowIndex++)
+            {
+                for (var smudgeColumnIndex = 0; smudgeColumnIndex < List[smudgeRowIndex].Length; smudgeColumnIndex++)
+                {
+                    List<string> tempList = new List<string>(List);
+
+                    var tempCharArray = tempList[smudgeRowIndex].ToCharArray();
+                    tempCharArray[smudgeColumnIndex] = tempCharArray[smudgeColumnIndex] == '.' ? '#' : '.';
+                    tempList[smudgeRowIndex] = new string(tempCharArray);
+
+                    int mirrorIndexLeft = int.MinValue;
+                    int mirrorIndexRight = int.MinValue;
+
+                    HashSet<string> mirrorSet = new HashSet<string>();
+
+                    for (var index = 0; index < tempList.Count; index++)
+                    {
+                        string line = tempList[index];
+
+                        if (!mirrorSet.Contains(line))
+                        {
+                            mirrorSet.Add(line);
+                            continue;
+                        }
+
+                        mirrorIndexLeft = index - 1;
+                        mirrorIndexRight = index;
+
+                        while (true)
+                        {
+                            if (mirrorIndexLeft < 0 || mirrorIndexRight >= tempList.Count)
+                            {
+                                tempSolution = isRow ? 100 * index : index;
+                                break;
+                            }
+                            if (tempList[mirrorIndexLeft] == tempList[mirrorIndexRight])
+                            {
+                                mirrorIndexLeft--;
+                                mirrorIndexRight++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                        }
+                    }
                 }
             }
             partSolution += tempSolution;
